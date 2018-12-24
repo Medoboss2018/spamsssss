8a9fe599a9c744efecda9e70afa865ced91f3d6a
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-24 02:19:03 -0800

    Update and rename Spam.js to On.js

--- On.js
+++ On.js
@@ -24,32 +24,4 @@ client.user.setGame(`Hi Friend`,)
   console.log('')
 });
 
-var ServerID = "526697815678648331"; 
-var ChannelID = "526697919542460416";
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 0.5000);
- 
 client.login(process.env.USER_TOKEN);
