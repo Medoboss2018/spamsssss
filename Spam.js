55b80672e7d9ebc360e84a224511b9c45c5ca803
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-24 02:00:51 -0800

    Create Spam.js

---
+++ Spam.js
@@ -0,0 +1,55 @@
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`الشيء الي تبيه`,)
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+var ServerID = "526697815678648331"; 
+var ChannelID = "526697919542460416";
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 0.5000);
+ 
+client.login(process.env.USER_TOKEN);
